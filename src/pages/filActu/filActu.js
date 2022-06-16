7893f1f046bd3f227ed9cdd642e77feb28ddc5c0
Rabat martin <martin.rabat@gmail.com>
2022-06-16 12:13:53 +0200

    addfil actu

--- src/pages/filActu/filActu.js
+++ src/pages/filActu/filActu.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import Post from '../../components/Posts/Post';
-import { getPosts } from '../../lib/Social-Network-Library';
+import { getPosts,createPost,register,login } from '../../lib/Social-Network-Library/';
 
 
 function FilActu() {
 
     const getPost = async () => {
-        let posts = await getPosts(0, 20); // Utilisation de la fonction login
-        console.log(posts)
+        let posts = await getPosts(0, 20); 
+
+        console.table(posts)
     }
 
-    return ( <div>{getPost}</div> );
+    const createP = () => {
+        register("firstname", "lastname", "email", "password")
+        login('email', "password") 
+        createPost("TEST1", "UN SUPER TESTTTT")// Utilisation de la fonction login
+        //console.table(posts)
+    }
+
+    return ( <div>{getPost()}</div> );
 }
 
 export default FilActu;
