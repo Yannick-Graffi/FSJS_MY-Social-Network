bf13a3f8049df7f9307cbf6caab1ae0291c16060
Rabat martin <martin.rabat@gmail.com>
2022-06-16 12:31:32 +0200

    update filactu change post

--- src/pages/filActu/filActu.js
+++ src/pages/filActu/filActu.js
@@ -1,24 +1,11 @@
-import React from 'react';
-import Post from '../../components/Posts/Post';
-import { getPosts,createPost,register,login } from '../../lib/Social-Network-Library/';
+import React, { useState } from 'react';
 
 
 function FilActu() {
 
-    const getPost = async () => {
-        let posts = await getPosts(0, 20); 
-
-        console.table(posts)
-    }
-
-    const createP = () => {
-        register("firstname", "lastname", "email", "password")
-        login('email', "password") 
-        createPost("TEST1", "UN SUPER TESTTTT")// Utilisation de la fonction login
-        //console.table(posts)
-    }
-
-    return ( <div>{getPost()}</div> );
+    return ( <div>
+        un post
+    </div> );
 }
 
 export default FilActu;
