f37da13325e8b9aca659e181e2431e13c1d3aa1f
Yannick <yann001@hotmail.com>
2022-06-16 14:42:58 +0200

    Finalisation du menu et des liens de navigations

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -1,15 +1,31 @@
 import "../menu/menu.css"
-import {Link} from "react-router-dom";
+import {Link, useLocation} from "react-router-dom";
 
 
 function Menu() {
+    let location = useLocation();
+
     return (  
         <div>
-            <h1>MY Réseau Social</h1>
-            <div>
-                <Link to="/Login">Se connecter</Link>
-                <Link to="/Register">Créer un compter</Link>
-            </div>
+
+
+            { location.pathname === "/Actu"
+                ?
+                    <div className="menu-container">
+                        <Link className="titre" to={"/Actu"}>MY Réseau Social</Link>
+                        <Link to="/Login">Se déconnecter</Link>
+
+                    </div>
+                :
+                    <div className="menu-container">
+                        <h1 className="titre">MY Réseau Social</h1>
+                        <div>
+                            <Link to="/Login">Se connecter</Link>
+                            <Link to="/Register">Créer un compte</Link>
+                        </div>
+                    </div>
+
+            }
         </div>
     );
 }
